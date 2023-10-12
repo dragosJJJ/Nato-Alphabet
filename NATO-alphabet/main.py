4caab432022c8adfef9fe4df1fc6ffe2f03bba7c
dragosJJJ <146551018+dragosJJJ@users.noreply.github.com>
2023-10-12 21:26:03 +0300

    main.py

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -3,8 +3,16 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
 
-word = input()
-ans = [phonetic_dict[letter.upper()] for letter in word]
-print(ans)
+def inp():
+    word = input()
+    try:
+        ans = [phonetic_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print("Please use only letters from the alphabet!")
+        inp()
+    else:
+        print(ans)
+
+inp()
 
 
